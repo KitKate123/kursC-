3ae9a9d705f9a15cd5e70b37bdab7e6e65976fbe
KitKate123 <katesh93@gmail.com>
2015-05-20 21:11:17 +0300

    Добавлено сравнение размера файла с местом на диске, где размещен файл

--- ITPlace/Program.cs
+++ ITPlace/Program.cs
@@ -12,21 +12,25 @@ namespace ITPlace
         static void Main(string[] args)
         {
             int number = 0;
+            Console.WriteLine("Пропишите путь, где хотите сохранить html - файлы");
+            string path = Console.ReadLine();
+            ///Console.WriteLine(path);
             // Создадим первый html - файл
-            using (StreamWriter htmlFile = new StreamWriter("index"+number+".html", false, System.Text.Encoding.Default))
+            using (StreamWriter htmlFile = new StreamWriter(path+"index"+number+".html", false, System.Text.Encoding.Default))
             {
                 htmlFile.WriteLine("<!DOCTYPE html>");
                 htmlFile.WriteLine("<html> <body> <p>");
             }
             //Считывание файла с текстом
             int n = -1;
+            
             using (StreamReader sText = new StreamReader("text.txt", System.Text.Encoding.Default))
             {
                 string readText = "";
                 while ((readText = sText.ReadLine()) != null)
                 {
                     n++;
-                    Console.WriteLine(readText);
+                    //Console.WriteLine(readText);
                     //Cчитывание файла - словаря
                     using (StreamReader sWords = new StreamReader("words.txt", System.Text.Encoding.Default))
                     {
@@ -36,12 +40,13 @@ namespace ITPlace
                             //Проверка на наличие слова из словаря в предложении
                             if (readText.Contains(readWords))
                             {
-                                Console.WriteLine("Совпало  " + readWords);
+                                //Console.WriteLine("Совпало  " + readWords);
                                 //ищется начальный индекс слова, встречающегося в предложении
                                 int position = readText.IndexOf(readWords);
-                                Console.WriteLine(position);
+                                ///Console.WriteLine(position);
+                                ///
                                 //запись строки в новый файл
-                                using (StreamWriter sw = new StreamWriter("index"+number+".html", true, System.Text.Encoding.Default))
+                                using (StreamWriter sw = new StreamWriter(path+"index"+number+".html", true, System.Text.Encoding.Default))
                                 {
                                     sw.Write(readText.Substring(0, position));
                                     sw.Write("<strong> <i>" + readText.Substring(position,readWords.Length) + "</i> </strong>");
@@ -55,9 +60,22 @@ namespace ITPlace
                     if (n > 90) //Если количество строк в html-файле превышает число 90
                     {
                         //закрываем файл
-                        using (StreamWriter sw = new StreamWriter("index"+number+".html", true, System.Text.Encoding.Default))
+                        using (StreamWriter sw = new StreamWriter(path+"index"+number+".html", true, System.Text.Encoding.Default))
                         {
                             sw.WriteLine("</p> </body> </html>");
+                            ///Проверим место на диске и сравним с размером файла
+                            FileInfo f = new FileInfo(path + "index" + number + ".html");
+                            double sizef = (f.Length / 1024) / 1024;
+                            string Vol = "";
+                            DriveInfo di = new DriveInfo(path);
+                            double Ffree = (di.AvailableFreeSpace / 1024) / 1024;
+                            if (sizef > Ffree)
+                            {
+                                Console.WriteLine("Нет места на диске!");
+                                break;
+                            }
+                            Console.WriteLine(Ffree.ToString("#,##") + " MB");
+                            Console.WriteLine(Vol);
                             sw.Close();
                         }
                         //Увеличиваем счетчик файлов
@@ -65,7 +83,7 @@ namespace ITPlace
                         //обнуляем количество строк для нового файла
                         n = 0;
                         //создаем новый html - файл
-                        using (StreamWriter htmlFile = new StreamWriter("index" + number + ".html", false, System.Text.Encoding.Default))
+                        using (StreamWriter htmlFile = new StreamWriter(path+"index" + number + ".html", false, System.Text.Encoding.Default))
                         {
                             htmlFile.WriteLine("<!DOCTYPE html>");
                             htmlFile.WriteLine("<html> <body> <p>");
@@ -73,7 +91,7 @@ namespace ITPlace
                     }
                }
                 //если не превышает, по окончанию цикла просто закрываем файл
-                using (StreamWriter sw = new StreamWriter("index" + number + ".html", true, System.Text.Encoding.Default))
+                using (StreamWriter sw = new StreamWriter(path+"index" + number + ".html", true, System.Text.Encoding.Default))
                 {
                     sw.WriteLine("</p> </body> </html>");
                     sw.Close();
@@ -81,6 +99,8 @@ namespace ITPlace
                 sText.Close();
 
             }
+            Console.WriteLine("Для выхода из программы нажмите Enter");
+            Console.ReadKey();
         }
     }
 }
